58b800f1b0eef887a8a6f8d990979861182272cb
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-07-31 21:29:22 -0400

    glUniform3f can make the bond between vec3 and vec4

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -12,10 +12,12 @@ const unsigned int SCR_HEIGHT = 600;
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "layout (location = 1) in vec3 aColor;\n"
+    "uniform vec3 offset;\n"
     "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vec3 result = offset + aPos;\n"
+    "   gl_Position = vec4(result, 1.0);\n"
     "   ourColor = aColor;\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
@@ -97,6 +99,9 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    int vertexOffset = glGetUniformLocation(shaderProgram, "offset");
+    glUseProgram(shaderProgram);
+    glUniform3f(vertexOffset, 0.3f, 0.3f, 0.3f);
 
     float vertices[] = {
         // positions         // colors
