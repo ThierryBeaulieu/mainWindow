a48aee876a5975d920f57cba058321e24efbd423
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-08-06 16:54:10 -0400

    print basic rectangle

--- src/shaders/shader.fs
+++ src/shaders/shader.fs
@@ -1,12 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoord;
-
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+in vec3 ourColor;
 
 void main()
 {
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+	FragColor = vec4(ourColor, 1.0f);
 }
